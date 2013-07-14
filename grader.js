85ef623ef72684a4d0dc66d8833dcfbac3dc8be7
James Smith <james.smith@cantab.net>
2013-07-14 00:05:40 +0000

    completed part3

--- grader.js
+++ grader.js
@@ -20,18 +20,32 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
-
-var fs = require('fs');
-var program = require('commander');
 var cheerio = require('cheerio');
+var program = require('commander');
+var fs = require('fs');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://afternoon-wave-5093.herokuapp.com";
+
+
+var assertUrlExists = function(url) {
+    rest.get(url).on('complete', function(result) {
+        if (result instanceof Error) {
+            console.log('Error: ' + result.message);
+            this.retry(5000); // try again after 5 sec
+        } else {
+            // console.log(result);
+        }
+    });
+};
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        console.log("%s does not exist. Exiting.", infile);
+        process.exit(1); // https://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -42,33 +56,33 @@ var cheerioHtmlFile = function(htmlfile) {
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
-};
+}
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
-    for(var ii in checks) {
+    for( var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
     return out;
-};
+}
 
 var clone = function(fn) {
-    // Workaround for commander.js issue.
-    // http://stackoverflow.com/a/6772648
+    // Workaround for commander.js issue -> stackoverflow.com/a/6772648
     return fn.bind({});
-};
+}
 
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to the site to check', clone(assertUrlExists), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
